fef83abddee53d9d6f3a64e51f491febdf575b5f
Dragos Ursu <u_dragos@yahoo.com>
2013-03-13 19:50:36 +0200

    _super/_parent fix + added _private data

--- js/jquery.aciPlugin.js
+++ js/jquery.aciPlugin.js
@@ -1,6 +1,6 @@
 
 /*
- * aciPlugin little jQuery plugin helper v1.0.0
+ * aciPlugin little jQuery plugin helper v1.1.0
  * http://acoderinsights.ro
  *
  * Copyright (c) 2013 Dragos Ursu
@@ -8,14 +8,15 @@
  *
  * Require jQuery Library >= v1.2.3 http://jquery.com
  *
- * Date: Sun Mar 10 18:50 2013 +0200
+ * Date: Wed Mar 13 19:10 2013 +0200
  */
 
 /*
  * This is not a jQuery plugin but a helper to help you create extendable
  * jQuery plugins. It provide the means to access instance data (with the
- * '_instance' property), you can access the parent overridden function
- * with '_super' and any other parent functions with '_parent'.
+ * '_instance' property), private data (with the '_private' property),
+ * you can access the parent overridden function with '_super' and any other
+ * parent functions with '_parent'.
  * The object is keept on the element with .data and there exists functionality
  * to get/set default options and getting access to plugin API.
  *
@@ -31,31 +32,49 @@
  * and 'defaultOptionsObject' is the default options object (will be available as $.fn.[yourPluginName].defaults).
  *
  * Extend a plugin like:
- *      aciPluginClass.plugins.[yourPluginName] = aciPluginClass.[yourPluginName].extend([yourPluginExtensionObject]);
- * where 'yourPluginName' is the plugin name and the 'yourPluginExtensionObject' is the extension object.
+ *      aciPluginClass.plugins.[yourPluginName] = aciPluginClass.[yourPluginName].extend([yourPluginExtensionObject], 'yourPluginExtensionName');
+ * where 'yourPluginName' is the plugin name, the 'yourPluginExtensionObject' is the extension object and the 'yourPluginExtensionName'
+ * is the extension name to be used to store private data (if any). Private data is stored as an object into
+ *      this._instance._private.[yourPluginExtensionName]
+ * and there is one default key set: 'nameSpace' equal with ('.' + yourPluginExtensionName). You can use this 'private' namespace
+ * for specific extension event (un)binding (for example).
  *
  * Extend the default options (add more options) with:
  *      aciPluginClass.defaults('[yourPluginName]', [extraOptionsObject]);
  * where 'yourPluginName' is the plugin name (same as the extended one)
  * and 'extraOptionsObject' is the extra options object to be added (to be available as $.fn.[yourPluginName].defaults).
  *
- * Access the current jQuery element with:
- *      this._instance.jQuery
+ * From within plugin methods and extension methods you can ...
+ *
+ * Access instance data ( do not override predefined reserved keys, see '__construct' and '__extend' !!! ):
+ *      this._instance.almostAnything = value;
+ *
+ * Access the current jQuery element:
+ *      this._instance.jQuery.css(style, value);
  *
  * Use the namespace (same as the plugin name with a dot in front):
  *      this._instance.jQuery.bind('click' + this._instance.nameSpace, [function]);
+ *      this._instance.jQuery.unbind(this._instance.nameSpace);
  *
  * Read the options:
  *      var option = this._instance.options.[option];
  * or use the 'options' function:
  *      var option = this.options('[option]');
  *
- * How to call the super/parent functions:
+ * Call the super/parent functions:
  *      this._super([argument1], [argument2], ... );
  *      this._parent.[function].apply(this, arguments);
  *      this._parent.[function].call(this, [argument1], [argument2], ... );
  *
- * Check the website pages for a quick plugin sample ;)
+ * Use extension private data ( do not forget to pass 'extensionName' when calling '.extend' and
+ * do not override predefined reserved keys, see '__construct' and '__extend' !!! ):
+ *      this._private.almostAnything = value
+ *
+ * Use the private namespace (same as the extension name with a dot in front):
+ *      this._instance.jQuery.bind('click' + this._private.nameSpace, [function]);
+ *      this._instance.jQuery.unbind(this._private.nameSpace);
+ *
+ * Check http://acoderinsights.ro/en/aciPlugin-a-helper-for-jQuery-plugin-creation for a simple plugin example.
  */
 
 (function($){
@@ -70,7 +89,7 @@
     this.aciPluginClass = function(){};
 
     // a basic extendable class
-    aciPluginClass.extend = function(properties){
+    aciPluginClass.extend = function(properties, extensionName){
         aciPluginClass.extend = arguments.callee;
         function aciPluginClass() {
             if (construct){
@@ -85,17 +104,36 @@
         var parent = this.prototype;
         for (var name in properties){
             // extend with the new object
-            aciPluginClass.prototype[name] = (typeof properties[name] == 'function') && (typeof parent[name] == 'function') ?
-            (function(name, callable){
+            aciPluginClass.prototype[name] = (typeof properties[name] == 'function') ?
+            (function(name){
                 return function() {
+                    // parent access
+                    var _parent = this._parent;
+                    this._parent = parent;
                     // super access
+                    var _super = this._super;
                     this._super = parent[name];
-                    return callable.apply(this, arguments);
+                    // private data
+                    var _private = this._private;
+                    if (extensionName){
+                        // need extension name to access private data
+                        var _entry = this._instance._private;
+                        if (typeof _entry[extensionName] == 'undefined') {
+                            _entry[extensionName] = {
+                                nameSpace: '.' + extensionName
+                            };
+                        }
+                        this._private = _entry[extensionName];
+                    }
+                    var result = properties[name].apply(this, arguments);
+                    // restore old properties
+                    this._parent = _parent;
+                    this._super = _super;
+                    this._private = _private;
+                    return result;
                 };
-            })(name, properties[name]) : properties[name];
+            })(name) : properties[name];
         }
-        // parent access
-        aciPluginClass.prototype._parent = parent;
         return aciPluginClass;
     };
 
@@ -106,7 +144,7 @@
             var nameSpace = '.' + pluginName;
             var object = jQuery.data(nameSpace);
             if (object){
-                // object already constructed, use the old one
+                // object already constructed, restore old data
                 this._instance = object._instance;
                 return object.__request(settings, option, value);
             }
@@ -114,9 +152,15 @@
             jQuery.data(nameSpace, this);
             // note: names defined here are reserved, also check the plugin '__extend' definition before you extend a plugin
             $.extend(this._instance, {
+                // this will keep private data into 'this._instance._private[extensionName]' (accessible as 'this._private' within plugin methods)
+                _private: {},
+                // plugin namespace
                 nameSpace: nameSpace,
+                // the jQuery element
                 jQuery: jQuery,
+                // keep plugin options
                 options: $.extend({}, $.fn[pluginName].defaults, (typeof settings == 'object') ? settings : {}),
+                // keep 'init' state
                 wasInit: false
             });
             // extend instance data (to keep states for example)
@@ -124,7 +168,8 @@
             return this.__request(settings, option, value);
         },
         __extend: function(){
-        // override this to add extra instance data on construct, like:
+        // override this to add extra instance/private data on construct
+        // do not override predefined reserved keys from '__construct' (and the base object incase of an extension)
         // $.extend(this._instance, {
         //        option1: [value],
         //        option2: [value]
